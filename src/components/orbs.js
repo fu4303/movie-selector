0c5683abc54b10f1bc7967cf6b2fd73bb4d2cf53
Luuk de Vlieger <luuk@trendwerk.nl>
2015-12-20 18:51:33 +0100

    Add orbs animation

--- src/components/orbs.js
+++ src/components/orbs.js
@@ -6,6 +6,11 @@
 
   class Orbs {
 
+    constructor() {
+      this.amount = 30;
+      this.duration = 60;
+    }
+
     createOrbs() {
       let sizes = [
         'small',
@@ -14,6 +19,24 @@
       ];
 
       let css = '';
+
+      for(let i = 1; i <= this.amount; i++) {
+        let randomLeftStart = input.randomize(101) - 1;
+        let randomTopStart = input.randomize(101) - 1;
+        let randomLeftEnd = input.randomize(101) - 1;
+        let randomTopEnd = input.randomize(101) - 1;
+
+        css += '@keyframes test' + i + '{';
+          css += '0% {';
+            css += 'left: ' + randomLeftStart + '%;';
+            css += 'top: ' + randomTopStart + '%;';
+          css += '}';
+          css += '100% {';
+            css += 'left: ' + randomLeftEnd + '%;';
+            css += 'top: ' + randomTopEnd + '%;';
+          css += '}';
+        css += '}';
+      }
 
       let orbs = document.createElement('div');
       let style = document.createElement('style');
@@ -25,17 +48,13 @@
 
       orbs.id = 'orbs';
 
-      for(let i = 0; i < 40; i++) {
+      for(let i = 1; i <= this.amount; i++) {
         let orb = document.createElement('div');
 
         let randomSize = input.randomize(sizes.length) - 1;
-        let randomLeft = input.randomize(101) - 1;
-        let randomTop = input.randomize(101) - 1;
 
         orb.className = 'orb ' + sizes[randomSize];
-        orb.style.left = randomLeft + '%';
-        orb.style.top = randomTop + '%';
-        orb.style.animation = randomTop + '%';
+        orb.style.animation = 'test' + i + ' ' + this.duration + 's ease-out forwards';
 
         orbs.appendChild(orb);
       }
