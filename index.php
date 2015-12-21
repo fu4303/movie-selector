6c9408c964eb9026f4101281c8ac3e4ae3e24fe8
Luuk de Vlieger <luuk@trendwerk.nl>
2015-12-21 18:23:38 +0100

    Margins

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 
 	<head>
 		<meta charset="utf-8">
-
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+		
 		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Arimo:400,700">
 		<link rel="stylesheet" type="text/css" href="dist/all.min.css">
 
