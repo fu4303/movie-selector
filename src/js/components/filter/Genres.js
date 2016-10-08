847dd51ba2a3514da6c42b6799de69a2a5370bcf
Luuk <info@luuk.site>
2016-10-08 14:37:53 +0200

    Toggle genres

--- src/js/components/filter/Genres.js
+++ src/js/components/filter/Genres.js
@@ -3,13 +3,15 @@ import store from '../../store.js';
 export default {
   template: `
     <div class="component">
-      <h3>Genres<span v-on:click="active = !active" v-bind:class="{'active': active}">{{ optionsText }}</span></h3>
+      <h3>Genres<span v-on:click="toggleShow()" v-bind:class="{'active': active}">{{ optionsText }}</span></h3>
 
       <transition name="slide">
         <ol v-show="active">
           <li v-for="genre in genres" v-on:click="toggleActive(genre.id)">
             <div class="check">
-              <span class="mark" v-show="isActive(genre.id)"></span>
+              <transition name="appear">
+                <span class="mark" v-show="isActive(genre.id)"></span>
+              </transition>
             </div>
 
             <p>{{ genre.name }}</p>
@@ -27,13 +29,22 @@ export default {
   },
   methods: {
     isActive: id => {
-      return false;
+      return store.state.active.genres.indexOf(id) !== -1;
     },
     toggleActive: id => {
       store.commit('toggleActive', {
         type: 'genres',
         id: id
       });
+    },
+    toggleShow: function() {
+      this.active = !this.active;
+
+      if (this.optionsText !== 'Show options') {
+        this.optionsText = 'Show options';
+      } else {
+        this.optionsText = 'Hide options';
+      }
     }
   }
 }
