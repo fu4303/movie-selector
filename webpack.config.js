a236ee741433071d444c9993820cb6a72d5c0b07
Luuk <info@luuk.site>
2016-12-17 12:56:49 +0100

    Production config

--- webpack.config.js
+++ webpack.config.js
@@ -4,59 +4,70 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-  entry:  './src/js/app.js',
-  module: {
-    rules: [
-      {
-        include: path.resolve(__dirname, 'src/js'),
-        loader: 'babel-loader',
-        options: {
-          compact: true,
-          presets: ['es2015'],
+module.exports = (env) => {
+  const config = {
+    entry:  './src/js/app.js',
+    module: {
+      rules: [
+        {
+          include: path.resolve(__dirname, 'src/js'),
+          loader: 'babel-loader',
+          options: {
+            compact: true,
+            presets: ['es2015'],
+          },
+          test: /\.js$/,
         },
-        test: /\.js$/,
-      },
-      {
-        include: path.resolve(__dirname, 'src/scss'),
-        loader: ExtractTextPlugin.extract({
-          fallbackLoader: 'style-loader',
-          loader: [
-            {
-              loader: 'css-loader',
-              query: {
-                autoprefixer: {
-                  add: true,
-                  browsers: ['> 0.05%'],
+        {
+          include: path.resolve(__dirname, 'src/scss'),
+          loader: ExtractTextPlugin.extract({
+            fallbackLoader: 'style-loader',
+            loader: [
+              {
+                loader: 'css-loader',
+                query: {
+                  autoprefixer: {
+                    add: true,
+                    browsers: ['> 0.05%'],
+                  },
                 },
-                minimize: true,
               },
-            },
-            {
-              loader: 'sass-loader',
-            },
-          ],
-        }),
-        test: /\.scss$/,
-      },
-    ],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist/js'),
-  },
-  plugins: [
-    new ExtractTextPlugin('../css/bundle.css'),
-    new webpack.optimize.UglifyJsPlugin({
-      output: {
-        comments: false,
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      vue: 'vue/dist/vue.js',
+              {
+                loader: 'sass-loader',
+              },
+            ],
+          }),
+          test: /\.scss$/,
+        },
+      ],
     },
-  },
-  watch: true,
-};
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist/js'),
+    },
+    plugins: [
+      new ExtractTextPlugin('../css/bundle.css'),
+    ],
+    resolve: {
+      alias: {
+        vue: 'vue/dist/vue.common.js',
+      },
+    },
+    watch: true,
+  };
+
+  if (env && env.production) {
+    config.plugins.push(
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false,
+        },
+        output: {
+          comments: false,
+        },
+      })
+    );
+  }
+
+  return config;
+}
