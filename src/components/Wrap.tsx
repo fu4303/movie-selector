46e469bdf9d3d924f45f9c9fbd92d826ddf1172c
Luuk de Vlieger <info@luuk.site>
2020-04-27 11:34:52 +0200

    :zap: Mobile background

--- src/components/Wrap.tsx
+++ src/components/Wrap.tsx
@@ -3,17 +3,37 @@ import React from 'react'
 export default ({
   children,
   background,
+  booting,
 }: {
   children: React.ReactNode
   background?: string | null
+  booting?: boolean
 }) => (
   <>
     <div
-      className="opacity-50 fixed w-full h-full left-0 top-0 bg-cover bg-center"
+      className={`
+        background
+        ${booting ? '' : 'bg-white'}
+        md:bg-transparent
+        md:opacity-50
+        fixed
+        w-full
+        h-full
+        left-0
+        top-0
+        bg-cover
+        bg-center
+      `}
       style={{ backgroundImage: background ? `url('${background}')` : 'none' }}
     ></div>
-    <div className="min-h-screen flex justify-center items-center flex-col">
-      <div className="relative flex justify-center items-center">
+    <div className={`
+      ${booting ? 'min-h-screen' : 'md:min-h-screen'}
+      flex
+      justify-center
+      items-center
+      flex-col
+    `}>
+      <div className="relative flex justify-center items-center h-full">
         {children}
       </div>
     </div>
