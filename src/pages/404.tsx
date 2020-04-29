41a23fba527b2626499bb35c20d32154beff41b1
Luuk de Vlieger <info@luuk.site>
2020-04-29 16:31:18 +0200

    :lipstick: 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import Wrap from '../components/Wrap'
+import Box from '../components/Box'
 
 export default () => (
   <Wrap>
-    <h1>404</h1>
+    <Box>
+      <h2 className="font-semibold text-2xl md:text-4xl mb-4 leading-tight">
+        That's a 404.
+      </h2>
+      <p className="md:text-lg leading-relaxed text-gray-500">
+        You could head back to the homepage.
+      </p>
+    </Box>
   </Wrap>
 )
